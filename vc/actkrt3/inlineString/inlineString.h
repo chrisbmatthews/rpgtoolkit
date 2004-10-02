a703fcec2891a6ca614dc7dccaea6836614f9f6b
ksniloc <ksniloc@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2004-10-02 18:11:52 +0000

    Modified by Colin
    + Improvements
    
    
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@439 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- vc/actkrt3/inlineString/inlineString.h
+++ vc/actkrt3/inlineString/inlineString.h
@@ -9,10 +9,16 @@
 ///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////
-// Prevent multiple inclusions of this class
+// Prevent multiple inclusions of this file
 ///////////////////////////////////////////////////////////////////////////
-#ifndef STRING_CLASS
-#define STRING_CLASS
+#ifndef STRING_CLASS_H
+#define STRING_CLASS_H
+
+///////////////////////////////////////////////////////////////////////////
+// Inlusions
+///////////////////////////////////////////////////////////////////////////
+#include <string>				//For the STL string
+#include <string.h>				//For strings
 
 ///////////////////////////////////////////////////////////////////////////
 // Definition of the inlineString class
@@ -20,36 +26,51 @@
 class inlineString
 {
 
+	//////////////////////////////////////////////////////
+	// Private scope
+	//////////////////////////////////////////////////////
 	private:
 
-		//The main contents of the inlineString is stored in
-		//in array of characters
+		//The main contents of the inlineString
+		//is stored in in array of characters.
+		//This array is statically dimensioned to
+		//4048 characters for speed reasons.
 		char contents[4048];
 
+	//////////////////////////////////////////////////////
+	// Public scope
+	//////////////////////////////////////////////////////
 	public:
 
-		//Returns values
-		char* value();
+		//mainipulation
+		int len();										// return length of the string
+		inlineString mid(int start, int length);		// get character(s) from the string
+		bool contains(char* text);						// check if we contain a pointer to a string
+		bool contains(std::string text);				// check if we contain an STL string
+		bool contains(inlineString text);				// check if we contain an inlineString
+		void newMem(char* theNewMem);					// copy string to new memory
 
-		//Manipulate the inlineString
-		int len();
-		inlineString mid(int start, int length);
-		int size();
-		void setBinary(unsigned char text, int pos);
-		char getBinary(int pos);		
-		bool contains(char* text);
-		void newMem(char* theNewMem);
+		//construction
+		inlineString();									// empty string
+		inlineString(char* defaultVal);					// construct from a pointer to a string
+		inlineString(std::string cFrom);				// construct from an STL string
+		inlineString(char defaultVal);					// construct from a character
 
-		//Construction
-		inlineString();
-		inlineString(char* defaultVal);
-
-		//Operator overloading
-		inlineString operator + (inlineString &toAdd);
-		inlineString operator + (char* toAdd);
-		inlineString operator + (char toAdd);
-		operator = (char* text);
-		operator = (char text);
+		//operators
+		inlineString operator + (inlineString &toAdd);	// + another inlineString
+		inlineString operator + (char* toAdd);			// + a pointer to a string
+		inlineString operator + (char toAdd);			// + a character
+		inlineString operator + (std::string toAdd);	// + an STL string
+		operator += (char* toAdd);						// += a pointer to a string
+		operator += (inlineString toAdd);				// += an inlineString
+		operator += (std::string toAdd);				// += an STL string
+		operator += (char toAdd);						// += a character
+		operator = (char* text);						// = a pointer to a string
+		operator = (char text);							// = a character
+		operator = (std::string cFrom);					// = an STL string
+		operator char*();								// cast to pointer to a string
+		operator std::string();							// cast to STL string
+		unsigned char& operator [] (int pos);			// get/set binary value
 
 };
 
