c8b1f3d305f5475a1eb6672bddacd16fb857929e
woozynl <woozynl@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2004-10-03 18:39:32 +0000

    no message
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@466 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- vc/actkrt3/tkrpgcode/parser.h
+++ vc/actkrt3/tkrpgcode/parser.h
@@ -60,6 +60,7 @@ void APIENTRY RPGCGetMethodName(VB_STRING);
 void APIENTRY RPGCParseAfter(VB_STRING, VB_STRING);
 void APIENTRY RPGCParseBefore(VB_STRING, VB_STRING);
 void APIENTRY RPGCGetVarList(VB_STRING, int);
+int APIENTRY RPGCStringContains(VB_STRING, VB_STRING);
 
 //////////////////////////////////////////////////////////////////////////
 // End of the file
