2233d5bfb93fd37dbf9588b418b0da36958bc7d9
ksniloc <ksniloc@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2004-12-24 04:30:34 +0000

    Updated and now with welcome note
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@936 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- build/regocx.bat
+++ build/regocx.bat
@@ -1,7 +1,35 @@
 @echo off
-echo Registering ocx controls...
-regsvr32 /s picclp32.ocx
-regsvr32 /s mci32.ocx
-regsvr32 /s mscomctl.ocx
-regsvr32 /s audiere.dll
-echo Done
+
+rem =====================================================
+rem All contents copyright 2004, Colin James Fitzpatrick
+rem All rights reserved. YOU MAY NOT REMOVE THIS NOTICE
+rem Read LICENSE.txt for licensing info
+rem =====================================================
+
+cls
+echo.
+echo RPGToolkit, Version 3 :: RegOcx
+echo -------------------------------
+echo.
+echo This file will register two components used
+echo by the RPGToolkit, Version 3 editor. This may fix
+echo problems with the code editor, or editor in its
+echo entirety, opening. The chances, however, aren't that
+echo high because the installer also performs this task.
+echo Therefore, this file is most useful if you have moved
+echo the toolkit to a new directory or you know of a reason
+echo why the installer would not have been able to register
+echo said components (ie. being logged under an account
+echo with insufficient capabilties). Regardless of your
+echo situation, running this file can do no harm, so you
+echo may as well try.
+echo.
+echo Registering the SSTab...
+regsvr32 /s TABCTL32.OCX
+echo Registering the rich text box...
+regsvr32 /s richtx32.ocx
+echo.
+echo Done!
+echo.
+echo [ Press any key to close this window ]
+pause > nul
