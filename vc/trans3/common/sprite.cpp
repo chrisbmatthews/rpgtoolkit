3f9dc8477f577945b1c37907b7326eaf0d8b8149
jdelano <jdelano@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2005-06-11 17:01:38 +0000

    Common attributes of players / sprites (Initial commit).
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@1278 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

---
+++ vc/trans3/common/sprite.cpp
@@ -0,0 +1,147 @@
+/*
+ * All contents copyright 2005 Christopher Matthews or Contributors.
+ * All rights reserved. You may not remove this notice
+ * Read license.txt for licensing details.
+ */
+
+/*
+ * Common sprite file attribute functions.
+ */
+
+#include "sprite.h"
+
+/*
+ * Get a stance animation filename
+ * (CommonPlayer playerGetStanceAnm and (CommonItem itemGetStanceAnm))
+ */
+std::string tagSpriteAttr::getStanceAnm(std::string stance)
+{
+    if (stance.empty())
+	{
+		stance = "WALK_S";
+	}
+	else
+	{
+		stance = parser::uppercase(stance);
+	}
+
+	std::string toRet = "";
+
+    if (stance == "STAND_S")
+	{
+		toRet = (standingGfx[PLYR_WALK_S].empty() ? gfx[PLYR_WALK_S] : standingGfx[PLYR_WALK_S]);
+    }
+    else if (stance == "STAND_N")
+	{
+		toRet = (standingGfx[PLYR_WALK_N].empty() ? gfx[PLYR_WALK_N] : standingGfx[PLYR_WALK_N]);
+    }
+    else if (stance == "STAND_E")
+	{
+		toRet = (standingGfx[PLYR_WALK_E].empty() ? gfx[PLYR_WALK_E] : standingGfx[PLYR_WALK_E]);
+    }
+    else if (stance == "STAND_W")
+	{
+		toRet = (standingGfx[PLYR_WALK_W].empty() ? gfx[PLYR_WALK_W] : standingGfx[PLYR_WALK_W]);
+    }
+    else if (stance == "STAND_NW")
+	{
+		// stand_nw > stand_w > walk_nw > walk_w.
+		toRet = (standingGfx[PLYR_WALK_NW].empty() ? standingGfx[PLYR_WALK_W] : standingGfx[PLYR_WALK_NW]);
+		if (toRet.empty())
+		{
+			toRet = (gfx[PLYR_WALK_NW].empty() ? gfx[PLYR_WALK_W] : gfx[PLYR_WALK_NW]);
+		}
+    }
+    else if (stance == "STAND_NE")
+	{
+		// stand_ne > stand_e > walk_ne > walk_e.
+		toRet = (standingGfx[PLYR_WALK_NE].empty() ? standingGfx[PLYR_WALK_E] : standingGfx[PLYR_WALK_NE]);
+		if (toRet.empty())
+		{
+			toRet = (gfx[PLYR_WALK_NE].empty() ? gfx[PLYR_WALK_E] : gfx[PLYR_WALK_NE]);
+		}
+    }
+    else if (stance == "STAND_SW")
+	{
+		// stand_sw > stand_w > walk_sw > walk_w.
+		toRet = (standingGfx[PLYR_WALK_SW].empty() ? standingGfx[PLYR_WALK_W] : standingGfx[PLYR_WALK_SW]);
+		if (toRet.empty())
+		{
+			toRet = (gfx[PLYR_WALK_SW].empty() ? gfx[PLYR_WALK_W] : gfx[PLYR_WALK_SW]);
+		}
+    }
+    else if (stance == "STAND_SE")
+	{
+		// stand_se > stand_e > walk_se > walk_e.
+		toRet = (standingGfx[PLYR_WALK_SE].empty() ? standingGfx[PLYR_WALK_E] : standingGfx[PLYR_WALK_SE]);
+		if (toRet.empty())
+		{
+			toRet = (gfx[PLYR_WALK_SE].empty() ? gfx[PLYR_WALK_E] : gfx[PLYR_WALK_SE]);
+		}
+    }
+	else if (stance == "WALK_S")
+	{
+		toRet = gfx[PLYR_WALK_S];
+	}
+	else if (stance == "WALK_N")
+	{
+		toRet = gfx[PLYR_WALK_N];
+	}
+	else if (stance == "WALK_E")
+	{
+		toRet = gfx[PLYR_WALK_E];
+	}
+	else if (stance == "WALK_W")
+	{
+		toRet = gfx[PLYR_WALK_W];
+	}
+	else if (stance == "WALK_NW")
+	{
+		toRet = (gfx[PLYR_WALK_NW].empty() ? gfx[PLYR_WALK_W] : gfx[PLYR_WALK_NW]);
+	}
+	else if (stance == "WALK_NE")
+	{
+		toRet = (gfx[PLYR_WALK_NE].empty() ? gfx[PLYR_WALK_E] : gfx[PLYR_WALK_NE]);
+	}
+	else if (stance == "WALK_SW")
+	{
+		toRet = (gfx[PLYR_WALK_SW].empty() ? gfx[PLYR_WALK_W] : gfx[PLYR_WALK_SW]);
+	}
+	else if (stance == "WALK_SE")
+	{
+		toRet = (gfx[PLYR_WALK_SE].empty() ? gfx[PLYR_WALK_E] : gfx[PLYR_WALK_SE]);
+	}
+	else if (stance == "FIGHT" || stance == "ATTACK")
+	{
+		toRet = gfx[PLYR_FIGHT];
+	}
+	else if (stance == "DEFEND")
+	{
+		toRet = gfx[PLYR_DEFEND];
+	}
+	else if (stance == "SPC" || stance == "SPECIAL MOVE")
+	{
+		toRet = gfx[PLYR_SPC];
+	}
+	else if (stance == "DIE")
+	{
+		toRet = gfx[PLYR_DIE];
+	}
+	else if (stance == "REST")
+	{
+		toRet = gfx[PLYR_REST];
+	}
+	else
+	{
+		//it's a custom stance, search the custom stances.
+		for (int i = 0; i <= customGfxNames.size(); i++)
+		{
+			if (parser::uppercase(customGfxNames[i]) == stance) 
+			{
+				toRet = customGfx[i];
+				break;
+			}
+		}
+	}
+    return toRet;
+}
---
+++ vc/trans3/common/sprite.h
@@ -0,0 +1,54 @@
+/*
+ * All contents copyright 2005 Christopher Matthews or Contributors.
+ * All rights reserved. You may not remove this notice
+ * Read license.txt for licensing details.
+ */
+
+/*
+ * Common sprite file attribute functions.
+ */
+
+#ifndef _SPRITE_H_
+#define _SPRITE_H_
+
+/*
+ * Includes.
+ */
+#include "../rpgcode/parser/parser.h"
+
+/*
+ * Definitions. *** Rename / move! ***
+ */
+
+#define PLYR_WALK_S 0
+#define PLYR_WALK_N 1
+#define PLYR_WALK_E 2
+#define PLYR_WALK_W 3
+#define PLYR_WALK_NW 4
+#define PLYR_WALK_NE 5
+#define PLYR_WALK_SW 6
+#define PLYR_WALK_SE 7
+#define PLYR_FIGHT 8
+#define PLYR_DEFEND 9
+#define PLYR_SPC 10
+#define PLYR_DIE 11
+#define PLYR_REST 12
+
+/*
+ * Common attributes of players and sprites:
+ * sprite vectors, speed variables.
+ */
+typedef struct tagSpriteAttr
+{
+	std::vector<std::string> gfx;				// Filenames of standard animations for graphics.
+	std::vector<std::string> standingGfx;		// Filenames of the standing animations/graphics.
+	std::vector<std::string> customGfx;			// Customized animations.
+	std::vector<std::string> customGfxNames;	// Customized animations (handles).
+	double idleTime;							// Seconds to wait proir to switching toSTAND_ graphics.
+	double speed;								// Seconds between each frame increase.
+//	int loopSpeed;	// Not needed here			// .speed converted to loops.
+
+	std::string getStanceAnm(std::string stance);
+} SPRITE_ATTR;
+
+#endif
