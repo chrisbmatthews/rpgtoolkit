302a63c0b6e1f8afc018b42ebdc40aab75fb67ce
ksniloc <ksniloc@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2006-05-14 03:49:46 +0000

    More virtual variables.
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@1640 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- vc/trans3/rpgcode/virtualvar.cpp
+++ vc/trans3/rpgcode/virtualvar.cpp
@@ -7,6 +7,8 @@
 #include "CProgram.h"
 #include "../movement/CPlayer/CPlayer.h"
 #include "../common/board.h"
+#include "../audio/CAudioSegment.h"
+#include "../misc/misc.h"
 #include <sstream>
 
 // These classes cannot have any of their own members, as their
@@ -49,7 +51,8 @@ STRING CVirtualNum::getLit() const
 	return ::getLit(*getPtr());
 }
 
-std::pair<int, int> getPlayerLocation(int tag)
+// <layer, <x, y>>
+std::pair<int, std::pair<int, int> > getPlayerLocation(int tag)
 {
 	extern std::vector<CPlayer *> g_players;
 	extern LPBOARD g_pBoard;
@@ -60,7 +63,7 @@ std::pair<int, int> getPlayerLocation(int tag)
 	}
 	catch (...)
 	{
-		return std::pair<int, int>(0.0, 0.0);
+		return std::pair<int, std::pair<int, int> >(0, std::pair<int, int>(0, 0));
 	}
 
 	const SPRITE_POSITION s = g_players[tag]->getPosition();
@@ -69,7 +72,7 @@ std::pair<int, int> getPlayerLocation(int tag)
 	int dx = int(s.x), dy = int(s.y);
 	coords::pixelToTile(dx, dy, g_pBoard->coordType, g_pBoard->bSizeX);
 
-	return std::pair<int, int>(dx, dy);
+	return std::pair<int, std::pair<int, int> >(s.l, std::pair<int, int>(dx, dy));
 }
 
 // Reserved variable: int playerX[idx]
@@ -77,7 +80,7 @@ class CPlayerLocationX : public tagStackFrame
 {
 public:
 	CPlayerLocationX(int idx) { tag = (void *)idx; }
-	double getNum() const { return getPlayerLocation(int(tag)).first; }
+	double getNum() const { return getPlayerLocation(int(tag)).second.first; }
 	STRING getLit() const { return ::getLit(getNum()); }
 	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
 };
@@ -87,7 +90,116 @@ class CPlayerLocationY : public tagStackFrame
 {
 public:
 	CPlayerLocationY(int idx) { tag = (void *)idx; }
-	double getNum() const { return getPlayerLocation(int(tag)).second; }
+	double getNum() const { return getPlayerLocation(int(tag)).second.second; }
+	STRING getLit() const { return ::getLit(getNum()); }
+	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
+};
+
+// Reserved variable: int playerLayer[idx]
+class CPlayerLocationZ : public tagStackFrame
+{
+public:
+	CPlayerLocationZ(int idx) { tag = (void *)idx; }
+	double getNum() const { return getPlayerLocation(int(tag)).first; }
+	STRING getLit() const { return ::getLit(getNum()); }
+	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
+};
+
+// Reserved variable: string playerHandle[idx]
+class CPlayerHandle : public tagStackFrame
+{
+public:
+	CPlayerHandle(int idx) { tag = (void *)idx; }
+	double getNum() const { return atof(getLit().c_str()); }
+	STRING getLit() const
+	{
+		extern std::vector<CPlayer *> g_players;
+		unsigned int iTag = (unsigned int)tag;
+		if (g_players.size() > iTag)
+		{
+			return g_players[iTag]->name();
+		}
+		return _T("");
+	}
+	UNIT_DATA_TYPE getType() const { return UDT_LIT; }
+};
+
+// Reserved variable: string music
+class CPlayingMusic : public tagStackFrame
+{
+public:
+	double getNum() const { return atof(getLit().c_str()); }
+	STRING getLit() const
+	{
+		extern CAudioSegment *g_bkgMusic;
+		return g_bkgMusic->getPlayingFile();
+	}
+	UNIT_DATA_TYPE getType() const { return UDT_LIT; }
+};
+
+// Reserved variable: int gameTime
+class CGameTime : public tagStackFrame
+{
+public:
+	double getNum() const
+	{
+		extern GAME_TIME g_gameTime;
+		return g_gameTime.gameTime();
+	}
+	STRING getLit() const { return ::getLit(getNum()); }
+	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
+};
+
+// Reserved variable: string boardTitle[idx]
+class CBoardTitle : public tagStackFrame
+{
+public:
+	CBoardTitle(int idx) { tag = (void *)idx; }
+	STRING getLit() const
+	{
+		extern LPBOARD g_pBoard;
+		return g_pBoard->boardTitle[int(tag)];
+	}
+	double getNum() const { return atof(getLit().c_str()); }
+	UNIT_DATA_TYPE getType() const { return UDT_LIT; }
+};
+
+// Reserved variable: double constant[idx]
+class CBoardConstant : public tagStackFrame
+{
+public:
+	CBoardConstant(int idx) { tag = (void *)idx; }
+	double getNum() const
+	{
+		extern LPBOARD g_pBoard;
+		return g_pBoard->brdConst[int(tag)];
+	}
+	STRING getLit() const { return ::getLit(getNum()); }
+	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
+};
+
+// Reserved variable: string boardBackground
+class CBoardBackground : public tagStackFrame
+{
+public:
+	STRING getLit() const
+	{
+		extern LPBOARD g_pBoard;
+		return g_pBoard->boardBackground;
+	}
+	double getNum() const { return atof(getLit().c_str()); }
+	UNIT_DATA_TYPE getType() const { return UDT_LIT; }
+};
+
+// Reserved variable: int boardSkill
+class CBoardSkill : public tagStackFrame
+{
+public:
+	double getNum() const
+	{
+		extern LPBOARD g_pBoard;
+		return g_pBoard->boardSkill;
+	}
 	STRING getLit() const { return ::getLit(getNum()); }
 	UNIT_DATA_TYPE getType() const { return UDT_NUM; }
 };
@@ -112,11 +224,46 @@ void setVirtualLit(const STRING name, STRING *pLit)
 	pFrame->tag = pLit;
 }
 
+/*
+ * Initialise virtual variables.
+ */
 void initVirtualVars()
 {
-	for (int i = 0; i < 5; ++i)
+	unsigned int i;
+	for (i = 0; i < 5; ++i)
 	{
-		CProgram::getGlobal("playerx[" + getLit(i) + "]") = new CPlayerLocationX(i);
-		CProgram::getGlobal("playery[" + getLit(i) + "]") = new CPlayerLocationY(i);
+		const STRING idx = getLit(i);
+
+		// Player location virtual variables.
+		CProgram::getGlobal(_T("playerx[") + idx + _T("]")) = new CPlayerLocationX(i);
+		CProgram::getGlobal(_T("playery[") + idx + _T("]")) = new CPlayerLocationY(i);
+		CProgram::getGlobal(_T("playerlayer[") + idx + _T("]")) = new CPlayerLocationZ(i);
+
+		// Player handles.
+		CProgram::getGlobal(_T("playerhandle[") + idx + _T("]")) = new CPlayerHandle(i);
 	}
+
+	// Board layer names.
+	for (i = 1; i < 9; ++i)
+	{
+		CProgram::getGlobal(_T("boardtitle[") + getLit(i) + _T("]")) = new CBoardTitle(i);
+	}
+
+	// Board constants.
+	for (i = 0; i < 11; ++i)
+	{
+		CProgram::getGlobal(_T("constant[") + getLit(i) + _T("]")) = new CBoardConstant(i);
+	}
+
+	// Playing music.
+	CProgram::getGlobal(_T("music")) = new CPlayingMusic();
+
+	// Seconds since game was started.
+	CProgram::getGlobal(_T("gametime")) = new CGameTime();
+
+	// Board fighting background.
+	CProgram::getGlobal(_T("boardbackground")) = new CBoardBackground();
+
+	// Board skill level.
+	CProgram::getGlobal(_T("boardskill")) = new CBoardSkill();
 }
