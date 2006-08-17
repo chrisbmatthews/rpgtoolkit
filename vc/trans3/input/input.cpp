cd112ceb44ae3a12607c053764bc790c7fedb178
ksniloc <ksniloc@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2006-08-17 05:42:02 +0000

    still better check for ToAscii() failure
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@1813 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- vc/trans3/input/input.cpp
+++ vc/trans3/input/input.cpp
@@ -360,7 +360,7 @@ LRESULT CALLBACK eventProcessor(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
 
 			// Get an ASCII representation of the key.
 			WORD key = 0;
-			if ((vir == VK_SHIFT) && !ToAscii(vir, scan, state, &key, 0))
+			if ((vir != VK_SHIFT) && !ToAscii(vir, scan, state, &key, 0))
 			{
 				// If ToAscii() failed, use the virtual code.
 				key = vir;
