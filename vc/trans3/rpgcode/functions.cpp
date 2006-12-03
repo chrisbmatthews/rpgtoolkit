0921169d89f5a772ea76421170b20dc4897de120
ksniloc <ksniloc@8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7>
2006-12-03 00:35:51 +0000

    Updated licence headers.
    git-svn-id: file:///home/cbm/dev/rpgtoolkit/svn/trunk@1962 8ad37bdc-5837-4682-a52a-fbb6d9f1e9f7

--- vc/trans3/rpgcode/functions.cpp
+++ vc/trans3/rpgcode/functions.cpp
@@ -1,7 +1,18 @@
 /*
- * All contents copyright 2005, 2006, Colin J. Fitzpatrick & Jonathan D. Hughes
- * All rights reserved. You may not remove this notice.
- * Read license.txt for licensing details.
+ ********************************************************************
+ * The RPG Toolkit, Version 3
+ * This file copyright (C) 2006  Colin Fitzpatrick & Jonathan Hughes
+ ********************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 /*
--- vc/trans3/trans3_i.c
+++ vc/trans3/trans3_i.c
@@ -5,7 +5,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Mon Sep 18 21:20:10 2006
+/* at Sat Dec 02 15:08:03 2006
  */
 /* Compiler settings for C:\cvs\vc\trans3\trans3.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
